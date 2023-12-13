0f0c94121ab7a19006108c2b8e788e694489551f
Alex Iraola <alex.iraola@gmail.com>
2023-12-14 00:34:15 +0100

    Spare calculation

--- src/tests/bowling.spec.ts
+++ src/tests/bowling.spec.ts
@@ -1,14 +1,63 @@
-function calculateResult(shots: string) {
-  return 0;
+function calculateScore(shots: string) {
+  const turns = shots.split(' ').map(turn => turn.replace(/-/g, '0'));
+
+  const isStrike = (turn: string) => turn === 'X';
+  const isSpare = (turn: string) => turn.includes('/');
+
+  const score = (turns: string[], index: number) => {
+    if (index >= turns.length) {
+      return 0;
+    }
+
+    const turn = turns[index];
+
+    if (isStrike(turn)) {
+      return 10;
+    }
+    if (isSpare(turn)) {
+      return 10;
+    }
+    return parseInt(turn[0]) + parseInt(turn[1]);
+  }
+
+  const turnsScore = (accScore: number, turns: string[], index: number) => {
+    if (index === turns.length) {
+      return accScore;
+    }
+    if (isSpare(turns[index])) {
+      const spareScore = score(turns, index) + score(turns, index + 1);
+
+      return turnsScore(accScore + spareScore, turns, index + 1);
+    }
+    return turnsScore(accScore + score(turns, index), turns, index + 1);
+  }
+
+  return turnsScore(0, turns, 0);
 }
 
 describe('Bowling', () => {
   it('should calculate a full fail correctly', () => {
     const shots = '-- -- -- -- -- -- -- -- -- --';
 
-    const result = calculateResult(shots);
+    const score = calculateScore(shots);
 
-    expect(result).toBe(0);
+    expect(score).toBe(0);
+  });
+
+  it('should calculate points correctly', () => {
+    const shots = '11 11 11 11 11 11 11 11 11 11';
+
+    const score = calculateScore(shots);
+
+    expect(score).toBe(20);
+  });
+
+  it('should calculate spare correctly', () => {
+    const shots = '5/ 5- -- -- -- -- -- -- -- --';
+
+    const score = calculateScore(shots);
+
+    expect(score).toBe(20);
   });
 
 });
